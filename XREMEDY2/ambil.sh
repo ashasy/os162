6286ca5052a9132fa17b3b424ab3e7dc16d19ea5
Dwitri Rhaiza Adiwidjaja - 1406623410 <dwitri.rhaiza@badak.cs.ui.ac.id>
2016-12-29 22:41:36 +0700

    uas rem babak 3

---
+++ XREMEDY2/ambil.sh
@@ -0,0 +1,14 @@
+# (c) 2016 Dwitri Rhaiza A. -- This is free software 
+# Feel free to copy and/or modify and/or distribute it,
+# provided this notice, and the copyright notice, are preserved.
+# REV01 Thu Dec 29 22:35:14 WIB 2016
+# START Thu Dec 29 11:30:55 WIB 2016
+# script menyimpan webpage dalam format text, mengambil konten 
+# yang berada diantara string ZCZC dan NNNN, dan menghapus line yang mengandung [DISK] 
+
+page="$(w3m -dump -cols 120 http://os162.vlsm.org/2016/11/ranking-os162.html)"
+
+echo "================================================== START BERKAS" > ranking.txt
+echo "$page" | awk '/ZCZC/{f=1;next} /NNNN/{f=0} f' | sed '/DISK/,+0 d' >> ranking.txt
+echo "================================================== STOP BERKAS" >> ranking.txt
+ 
