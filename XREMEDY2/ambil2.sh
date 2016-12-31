8887e10356f08a9eb8679df15f86c34578f5bba4
Dwitri Rhaiza Adiwidjaja - 1406623410 <dwitri.rhaiza@badak.cs.ui.ac.id>
2016-12-31 15:54:03 +0700

    delete start&stop berkas

---
+++ XREMEDY2/ambil2.sh
@@ -0,0 +1,4 @@
+# (c) 2016 Dwitri Rhaiza A. -- This is free software
+# Feel free to copy and/or modify and/or distribute it,
+# provided this notice, and the copyright notice, are preserved.
+# START Sat Dec 31 15:53:44 WIB 2016
